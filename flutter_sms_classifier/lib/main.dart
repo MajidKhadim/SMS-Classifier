4026def5730b8db619b9ffd28cea9efccdccc512
Majid Khadim <majidkhadim768@gmail.com>
2024-06-05 11:42:43 +0500

    made changes in flutter UI

--- flutter_sms_classifier/lib/main.dart
+++ flutter_sms_classifier/lib/main.dart
@@ -27,13 +27,12 @@ class _MessageClassificationScreenState extends State<MessageClassificationScree
 
   Future<void> _classifyMessage() async {
     final message = _controller.text;
-    final url = Uri.parse('http://10.0.2.2:5000/classify');  // Update with your server address if needed
+    final url = Uri.parse('http://10.0.2.2:5000/classify'); 
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
       body: json.encode({'message': message}),
     );
-    print("request sent");
     if (response.statusCode == 200) {
       final responseData = json.decode(response.body);
       setState(() {
@@ -48,31 +47,59 @@ class _MessageClassificationScreenState extends State<MessageClassificationScree
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Message Classifier'),
-        backgroundColor: Colors.blue,
+        title: const Text('Message Classifier',style: TextStyle(
+          color: Color.fromARGB(179, 255, 255, 255)
+        ),),
+        backgroundColor: Color.fromARGB(227, 0, 3, 25),
       ),
-      body: Padding(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(227, 0, 3, 15), Color.fromARGB(227, 25, 32, 80)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: 'Enter your message',
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  labelText: 'Enter your message',
+                  labelStyle: TextStyle(color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.white24,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _classifyMessage,
-              child: const Text('Classify'),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              _classification.isEmpty ? "" : 'Classification: $_classification',
-              style: const TextStyle(fontSize: 20),
-            ),
-          ],
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: _classifyMessage,
+                icon: Icon(Icons.send),
+                label: const Text('Classify',style: TextStyle(color: Colors.white70),),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 90, 86, 90), 
+                  iconColor: Colors.white,// Button graycolor
+                  textStyle: TextStyle(color: Colors.white), // Text color
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                _classification.isEmpty ? "" : 'Classification: $_classification',
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
