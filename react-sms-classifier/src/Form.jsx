9030699f7948c62011594423ecd12259b9eb644e
Majid Khadim <majidkhadim768@gmail.com>
2024-06-05 11:43:32 +0500

    made changes in react UI

--- react-sms-classifier/src/Form.jsx
+++ react-sms-classifier/src/Form.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import './Form.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Form.css';
 
 export default function Form() {
-    let [message,setMessage] = useState('');
-    let [classification,setClassification] = useState('');
+    const [message, setMessage] = useState('');
+    const [classification, setClassification] = useState('');
 
-    const onInput = (ev)=>{
-        setMessage(ev.target.value)
+    const onInput = (ev) => {
+        setMessage(ev.target.value);
     }
-    const handleSubmit = (ev)=>{
+
+    const handleSubmit = (ev) => {
         ev.preventDefault();
     }
-    const handleClassification = async ()=>{
+
+    const handleClassification = async () => {
         const response = await axios.post('http://localhost:5000/classify', { message });
-        setClassification(response.data.classification === 0? "Ham":"spam");
+        setClassification(response.data.classification === 0 ? "Ham" : "Spam");
     }
-  return (
-    <form onSubmit={handleSubmit}>
-        <h2>Messege Classifier</h2>
-        <textarea name="msg" id="msg" placeholder='Enter your messege here' rows={6} cols={100} value={message} onChange={onInput}></textarea>
-        <br/>
-        <button type='submit' onClick={handleClassification}>Predict</button>
-        {classification && <h4>Your Messege is {classification.toUpperCase()}</h4>}
-    </form>
-  )
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit} className="classification-form">
+                <h2>Message Classifier</h2>
+                <textarea 
+                    name="msg" 
+                    id="msg" 
+                    placeholder="Enter your message here" 
+                    rows={6} 
+                    value={message} 
+                    onChange={onInput} 
+                />
+                <br />
+                <button type="submit" onClick={handleClassification}>Predict</button>
+                {classification && <h4>Your Message is {classification.toUpperCase()}</h4>}
+            </form>
+        </div>
+    );
 }
